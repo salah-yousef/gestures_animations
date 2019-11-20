df0c8fe55fa80932505b9898d90263653c2293d8
salah_yousef <salah.yousef@pharmatolia.com>
2019-11-20 13:21:03 +0300

    Gestures

--- lib/main.dart
+++ lib/main.dart
@@ -7,58 +7,99 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Gestures and Animations',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int numTaps = 0;
+  int numDoubleTaps = 0;
+  int numLongPress = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  double posX = 0.0;
+  double posY = 0.0;
+  double boxSize = 150.0;
 
   @override
   Widget build(BuildContext context) {
+    if (posX == 0.0) {
+      center(context);
+    }
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Gestures and Animations"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            numTaps++;
+          });
+        },
+        onDoubleTap: () {
+          setState(() {
+            numDoubleTaps++;
+          });
+        },
+        onLongPress: () {
+          setState(() {
+            numLongPress++;
+          });
+        },
+        onVerticalDragUpdate: (DragUpdateDetails value) {
+          setState(() {
+            posY = posY + value.delta.dy;
+          });
+        },
+        onHorizontalDragUpdate: (DragUpdateDetails value) {
+          setState(() {
+            posX = posX + value.delta.dx;
+          });
+        },
+        child: Stack(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            Positioned(
+              left: posX,
+              top: posY,
+              child: Container(
+                width: boxSize,
+                height: boxSize,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                ),
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      bottomNavigationBar: Material(
+        color: Theme.of(context).primaryColorLight,
+        child: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Text(
+            "Taps: $numTaps \nDouble Taps: $numDoubleTaps \nLong Presses: $numLongPress",
+            style: Theme.of(context).textTheme.title,
+          ),
+        ),
       ),
     );
+  }
+
+  void center(BuildContext context) {
+    posX = (MediaQuery.of(context).size.width / 2) - boxSize / 2;
+    posY = (MediaQuery.of(context).size.height / 2) - boxSize / 2 - 30.0;
+    setState(() {
+      posX = posX;
+      posY = posY;
+    });
   }
 }
